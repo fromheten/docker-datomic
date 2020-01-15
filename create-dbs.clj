c871863f1d97c494998e4d365595700448915ba0
Jeroen de Jong <thumbnail@users.noreply.github.com>
2020-01-15 15:35:39 +0100

    Update create-dbs.clj
    Co-Authored-By: Joep Kemperman <joep.kemperman@nedap.com>

--- create-dbs.clj
+++ create-dbs.clj
@@ -11,5 +11,5 @@
                    (set))]
   (run! (fn [name] (d/create-database (str uri name "?password=123")))
         db-names)
-  (when-let [dbs (seq (set/difference db-names (-> (str uri "?password=123") d/get-database-names set)))]
+  (when-let [dbs (seq (set/difference db-names (-> (str uri "*?password=123") d/get-database-names set)))]
     (throw (ex-info "Unable to create some databases" {:databases dbs}))))
