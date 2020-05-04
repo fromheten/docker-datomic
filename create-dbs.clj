1e4a56cbeff3b23b13196ec855b9e85bab73cc61
Joep Kemperman <joep.kemperman@nedap.com>
2020-05-04 16:38:26 +0200

    Retry creation of databases (#4)
    * Retry creation of databases
    
    * Add another version level
    
    Not very semver compliant, but it allows us to release multiple versions
    for the same datomic version. For example:
    
    0.9.123.0
    0.9.123.1
    0.9.123.2
    
    * Add source of retry fn
    
    * Remove retry timeout
    
    It's not necessary, as the datomic calls already wait a while
    
    * Don't retry create databases, retry connecting instead

--- create-dbs.clj
+++ create-dbs.clj
@@ -3,12 +3,28 @@
  '[clojure.string :as str]
  '[clojure.set :as set])
 
+;; Source: https://stackoverflow.com/a/12068946
+(defn retry
+  "Retries the given function call until no exceptions are thrown."
+  [retries f & args]
+  (let [res (try {:value (apply f args)}
+                 (catch Exception e
+                   (if (zero? retries)
+                     (throw e)
+                     {:exception e})))]
+    (if (:exception res)
+      (recur (dec retries)f args)
+      (:value res))))
+
 (let [uri      "datomic:dev://localhost:4334/"
       db-names (-> (System/getenv "DATABASES")
                    (str)
                    (str/split #"\,")
                    (as-> names (remove str/blank? names))
                    (set))]
+  ;; Datomic may not yet have fully started at this point, so we must retry a couple of times.
+  ;; We fetch database names to ensure datomic is available, not because it's actually useful to do so.
+  (retry 5 d/get-database-names "datomic:dev://localhost:4334/*?password=123")
   (run! (fn [name] (d/create-database (str uri name "?password=123")))
         db-names)
   (when-let [dbs (seq (set/difference db-names (-> (str uri "*?password=123") d/get-database-names set)))]
