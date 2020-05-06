cd6b597284b34539d07cb206e006c4802a7379f1
Joep Kemperman <joep.kemperman@nedap.com>
2020-05-06 09:24:47 +0200

    Retry create-dbs with sleeps (#5)
    * Retry creation of db's with sleeping inbetween retry
    
    * Add debug info
    
    * Retry database creation, not database name fetching
    
    * Fix incorrect pprint
    
    * Remove debug pprints

--- create-dbs.clj
+++ create-dbs.clj
@@ -6,14 +6,16 @@
 ;; Source: https://stackoverflow.com/a/12068946
 (defn retry
   "Retries the given function call until no exceptions are thrown."
-  [retries f & args]
+  [retries timeout f & args]
   (let [res (try {:value (apply f args)}
                  (catch Exception e
                    (if (zero? retries)
                      (throw e)
                      {:exception e})))]
     (if (:exception res)
-      (recur (dec retries)f args)
+      (do
+        (Thread/sleep timeout)
+        (recur (dec retries) timeout f args))
       (:value res))))
 
 (let [uri      "datomic:dev://localhost:4334/"
@@ -23,9 +25,7 @@
                    (as-> names (remove str/blank? names))
                    (set))]
   ;; Datomic may not yet have fully started at this point, so we must retry a couple of times.
-  ;; We fetch database names to ensure datomic is available, not because it's actually useful to do so.
-  (retry 5 d/get-database-names "datomic:dev://localhost:4334/*?password=123")
-  (run! (fn [name] (d/create-database (str uri name "?password=123")))
-        db-names)
+  (retry 15 1000 run! (fn [name] (d/create-database (str uri name "?password=123")))
+                      db-names)
   (when-let [dbs (seq (set/difference db-names (-> (str uri "*?password=123") d/get-database-names set)))]
     (throw (ex-info "Unable to create some databases" {:databases dbs}))))
